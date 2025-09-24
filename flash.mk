d4d15b001239648e14241aa529ce7c7822097452
珞珞珞林 <59970710+wele0612@users.noreply.github.com>
2025-09-23 20:13:46 -0700

    STM32H733 project

---
+++ flash.mk
@@ -0,0 +1,29 @@
+# Author: Liwei Xue 2024.3.20
+# Flash script for makefile project, using OPENOCD.
+
+OCDSCRIPT_PATH = $(subst \,/,$(OPENOCD_HOME))/share/openocd/scripts
+#On windows "\" in file path will cause problems in OPENOCD
+
+PROJECT = RM25_firmware
+#TARGET = stm32h7x_noreset
+TARGET = stm32h7x
+INTERFACE = stlink-v2
+
+OCD = openocd
+ELF = build/$(PROJECT).elf
+
+default: load
+
+load: build_code
+	$(OCD) -f $(OCDSCRIPT_PATH)/interface/$(INTERFACE).cfg \
+	 -f  $(OCDSCRIPT_PATH)/target/$(TARGET).cfg \
+	 -c "program $(ELF) verify reset exit"
+
+build_code:
+	make -C ./ -j
+
+showpath:
+	@echo $(OPENOCD_HOME)
+	@echo $(OCDSCRIPT_PATH)
+
+.PHONY: build_code load
