2b205f4dcd615d0be3c6ebaa71a288207cff8d4f
Leo <87925599+LeoCalculus@users.noreply.github.com>
2026-01-10 13:40:49 -0800

    gimbal update

--- Application/Src/platform/gimbal.c
+++ Application/Src/platform/gimbal.c
@@ -1,16 +1,17 @@
 #include<config.h>
 #ifdef CONFIG_PLATFORM_GIMBAL
 
-void controller_init(){
 
+void controller_init(){
+    role_controller_init();
 }
 
 void controller_cycle(const float CTRL_DELTA_T){
-    
+    role_controller_step(CTRL_DELTA_T);
 }
 
 void dr16_on_change(){
-
+    
 }
 
 #endif
